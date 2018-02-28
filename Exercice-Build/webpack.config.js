96304d0804fe632b88530ce4eece5cde2507ee52
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2018-02-28 14:03:50 +0100

    WIP: Migration Webpack 4

--- Exercice-Build/webpack.config.js
+++ Exercice-Build/webpack.config.js
@@ -1,47 +1,32 @@
-const path = require('path');
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const UglifyJSWebpackPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = (env) => {
-
-  const plugins = [
+module.exports = {
+  entry: './src/js/index',
+  output: {
+    filename: 'app.[hash].js',
+  },
+  plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
     }),
-  ];
-
-  if (env === 'production') {
-    plugins.push(new UglifyJSWebpackPlugin());
+  ],
+  module: {
+    rules: [{
+      test: /\.json5$/,
+      loader: 'json5-loader'
+    }, {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      options: {
+        plugins: ['syntax-dynamic-import'],
+        presets: [['env', {
+          modules: false,
+          targets: {
+            browsers: ['Chrome 62']
+          }
+        }]]
+      }
+    }],
   }
-
-
-  return {
-    devtool: (env !== 'production') ? 'source-map' : false,
-    entry: './src/js/index',
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'app.[hash].js',
-    },
-    plugins,
-    module: {
-      rules: [{
-        test: /\.json5$/,
-        loader: 'json5-loader'
-      }, {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          plugins: ['syntax-dynamic-import'],
-          presets: [['env', {
-            modules: false,
-            targets: {
-              browsers: ['IE 11']
-            }
-          }]]
-        }
-      }],
-    }
-  };
 };
